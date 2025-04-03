f032c94400e80c5f391783e1ed99cc437085624e
Yu Waddy <yuwaddylwin.2004@gmail.com>
2025-04-03 17:52:22 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('pages/home', { title: 'Yu Waddy Lwin | Computer Science Portfolio' });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
